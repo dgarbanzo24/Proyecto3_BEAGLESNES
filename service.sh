6d17cc48b316909fed42beb7037fe41e10450bc7
Carlos <carlos92@carlos92.(none)>
2014-10-23 16:50:32 -0600

    Llamada SNES y activación video

---
+++ service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                                                                     
+export DISPLAY=:0                                                               
+./snes9x-sdl -conf /boot/uboot/beaglesnes/snes9x.conf &                         
+sleep 10   
